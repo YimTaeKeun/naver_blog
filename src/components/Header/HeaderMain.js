ca1090c3cabc52042c2bd0091ad7201a2ba5d2a6
YimTaekeun <ytk030305@gmail.com>
2023-11-05 02:28:35 +0900

    Feat: search nav bar design complete

--- src/components/Header/HeaderMain.js
+++ src/components/Header/HeaderMain.js
@@ -1,6 +1,15 @@
+import blog_logo from '../../imgSrc/blog_logo.jpg';
+import searchBtn from '../../imgSrc/searchBtn.jpg';
 const HeaderMain = () => {
     return <div className="HeaderMain">
-        <h3>This area is Header</h3>
+        <div className="blogSearch">
+            <a href="https://naver.com"><img src={blog_logo} style={{ width: '140px'}}/></a>
+            <input type="text" placeholder="검색하기" style={{width: '140px', height: '30px'}}/>
+            <img src={searchBtn} style={{width: '35px'}} className="searchBtn"/>
+        </div>
+        <div className="myProfile">
+
+        </div>
     </div>
 }
 export default HeaderMain;
